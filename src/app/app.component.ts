15e39ec2a4dfeeb13a6c6989600c56b0472a625c
Artur <helter@wp.pl>
2023-01-02 13:48:09 +0100

    create rgx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'rgx';
+  observable$: Observable<number> = of(1, 2, 3, 4, 5, 6);
+
+  subscription: Subscription | undefined;
+
+  private displayObservableNumbers(): void {
+    this.subscription = this.observable$.subscribe((value) =>
+      console.log(value)
+    );
+  }
+
+  ngOnInit(): void {
+    this.displayObservableNumbers();
+  }
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/shared/header/header.component';
+import { FooterComponent } from './modules/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
